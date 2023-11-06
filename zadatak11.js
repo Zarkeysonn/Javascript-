ab48b9bef39296a13c2c819d8e1671375c442062
Zarko Djurdjev <zarko.djurdjev@automaticity.rs>
2023-11-06 14:40:52 +0100

    Task 11

---
+++ zadatak11.js
@@ -0,0 +1,15 @@
+var a;
+var b;
+
+function doTheMath(a,b){
+    if(a < 0 && b < 0){
+        return "Ne dam ti da oba budu negativna :P"
+    }
+    else {
+        if(a > b) {
+            return a - b;
+        } else return b - a;
+    }
+}
+
+console.log(doTheMath(7, -17))
