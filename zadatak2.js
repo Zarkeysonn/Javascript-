35c567180d10986df39db9297d73fa0d8ccc0280
Zarko Djurdjev <zarko.djurdjev@automaticity.rs>
2023-11-06 12:40:46 +0100

    "Task 2 and 3 Done"

---
+++ zadatak2.js
@@ -0,0 +1,8 @@
+function zbir() {
+    var a = 5;
+    var b = 10;
+    var c = a + b;
+    return c;
+}
+
+console.log(zbir());
---
+++ zadatak3.js
@@ -0,0 +1,9 @@
+function mess() {
+    var a = 5;
+    var b = 10;
+    b = b + 3;
+    a = b;
+    return a;
+}
+
+console.log(mess());
