3d020071159b14fcd3adc73473aa9d9a8c2048e4
Zarko Djurdjev <zarko.djurdjev@automaticity.rs>
2023-11-06 13:18:41 +0100

    Task 4

---
+++ test.js
@@ -0,0 +1,5 @@
+const a = true;
+const b = 1;
+
+console.log(a == b);
+console.log(a === b);
---
+++ zadatak4.js
@@ -0,0 +1,18 @@
+var a = 5;
+var b = '5';
+
+function f() {
+    if ( a === b) {
+        return "Promenljive su jednake";
+    } else {
+        return "Promenljive nisu jednake";
+    }
+}
+
+console.log(a);
+console.log(b);
+console.log(a == b);
+console.log(a === b);
+console.log(f()); 
+
+
