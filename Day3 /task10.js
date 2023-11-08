0e54c271447a86181d4beeb0040c3f129128680c
Zarko Djurdjev <zarko.djurdjev@automaticity.rs>
2023-11-08 12:29:26 +0100

    Tasks 3-11

---
+++ Day3 /task10.js
@@ -0,0 +1,10 @@
+function sumArrayElements(){
+    let array = [1,2,3,4,5];
+    let sum = 0;
+    for(let i = 0;i < array.length; i++){
+        sum += array[i];
+    }
+    return sum;
+}
+
+console.log(sumArrayElements());
